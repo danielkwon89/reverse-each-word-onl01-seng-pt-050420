c2f7915192eb05acf3406b1dc662cb889407a088
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 23:58:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(string)
-  
+  string_array = string.split
+  final_array = []
+  string_array.each do |word|
+    final_array.push(word.reverse)
+  end
+  final_string = final_array.join(" ")
 end
