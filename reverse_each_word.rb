70788f767ef6558ad418ce4a4b745cfa42dfea08
Daniel Kwon <danielkwon89@gmail.com>
2020-05-11 00:25:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
+my_string = "Hello there, and how are you?"
+
 def reverse_each_word(string)
   string_array = string.split
   string_array.collect do |word|
-    word.reverse
+    string_array[string_array.index(word)] = word.reverse
   end
+  string_array.join(" ")
 end
